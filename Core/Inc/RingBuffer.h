aa8999412f7a11f931609977af0a69aadd23fe64
Mikolaj <mikolaj.malecki@thaumatec.com>
2020-10-13 19:47:34 +0200

    ring buf first

---
+++ Core/Inc/RingBuffer.h
@@ -0,0 +1,24 @@
+/*
+ * RingBuffer.h
+ *
+ *  Created on: Oct 13, 2020
+ *      Author: mikolaj
+ */
+
+#ifndef SRC_RINGBUFFER_H_
+#define SRC_RINGBUFFER_H_
+
+#include "stdint.h"
+
+typedef struct {
+	volatile char * const buffer;
+	uint8_t head;
+	uint8_t tail;
+} circ_buffer_t;
+
+
+uint8_t add(char *byte);
+uint8_t get(char *byte);
+
+
+#endif /* SRC_RINGBUFFER_H_ */
---
+++ Core/Src/RingBuffer.c
@@ -0,0 +1,34 @@
+/*
+ * RingBuffer.cpp
+ *
+ *  Created on: Oct 13, 2020
+ *      Author: mikolaj
+ */
+
+#include <RingBuffer.h>
+#define UART_BUF_SIZE 	50
+
+volatile char uart_buff[UART_BUF_SIZE];
+volatile circ_buffer_t uart_circ_buff = { uart_buff, 0, 0 };
+
+uint8_t add(char *byte){
+	uint8_t temp_head = uart_circ_buff.head + 1;
+
+	if (temp_head == UART_BUF_SIZE)	temp_head = 0;
+	if (temp_head == uart_circ_buff.tail)	return 0;
+
+	uart_circ_buff.buffer[temp_head] = *byte;
+	uart_circ_buff.head++;
+
+	return 1;
+};
+
+uint8_t get(char *byte){
+	if (uart_circ_buff.head == uart_circ_buff.tail) return 0;
+
+	uart_circ_buff.tail++;
+	if (uart_circ_buff.tail == UART_BUF_SIZE) uart_circ_buff.tail = 0;
+
+	*byte = uart_circ_buff.buffer[uart_circ_buff.tail];
+	return 1;
+}
